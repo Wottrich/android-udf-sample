17f97a621647190f69988b4995db3e5735f1f0fb
Lucas Cruz Wottrich <lucas.wottrich@ifood.com.br>
2022-02-18 17:29:38 -0300

    Trying create a observable state

--- ViewSystemUnidirectionalDataFlow/app/src/main/java/github/io/wottrich/myapplication/udfsample/initial/domain/ui/InitialUiState.kt
+++ ViewSystemUnidirectionalDataFlow/app/src/main/java/github/io/wottrich/myapplication/udfsample/initial/domain/ui/InitialUiState.kt
@@ -1,16 +1,61 @@
 package github.io.wottrich.myapplication.udfsample.initial.domain.ui
 
+import androidx.lifecycle.LiveData
 import github.io.wottrich.myapplication.textfield.TextFieldState
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class MutableStateLiveData<T: Any>(initialValue: T) : StateLiveData<T>(initialValue) {
+    public override fun postValue(value: T) {
+        super.postValue(value)
+    }
+
+    public override fun setValue(value: T) {
+        super.setValue(value)
+    }
+}
+
+open class StateLiveData<T : Any>(initialValue: T) : LiveData<T>(initialValue) {
+    override fun getValue(): T {
+        return checkNotNull(super.getValue())
+    }
+}
+
+abstract class ObservableState<T : Any>(initialState: T) {
+    private val _state = MutableStateLiveData<T>(initialState)
+    val state: StateLiveData<T> = _state
+
+    class StateChanger<T : Any, R>(
+        private val getter: T.() -> R,
+        private val setter: T.(R) -> T
+    ) : ReadWriteProperty<ObservableState<T>, R> {
+        override fun getValue(thisRef: ObservableState<T>, property: KProperty<*>): R {
+            return thisRef.state.value.getter()
+        }
+
+        override fun setValue(thisRef: ObservableState<T>, property: KProperty<*>, value: R) {
+            thisRef._state.value = thisRef.state.value.setter(value)
+        }
+    }
+}
+
+class InitialUiStateLiveDataImpl(initialValue: InitialUiState) : ObservableState<InitialUiState>(initialValue) {
+    var isLoading: Boolean by StateChanger(InitialUiState::isLoading) { copy(isLoading = it) }
+    var isConfirmButtonEnabled: Boolean by StateChanger(InitialUiState::isConfirmButtonEnabled) {
+        copy(isConfirmButtonEnabled = it)
+    }
+    var textFieldState: TextFieldState by StateChanger(InitialUiState::textFieldState) {
+        copy(textFieldState = it)
+    }
+}
 
 data class InitialUiState(
-    val name: String?,
     val isConfirmButtonEnabled: Boolean,
     val isLoading: Boolean,
     val textFieldState: TextFieldState
 ) {
     companion object {
         val Initial = InitialUiState(
-            name = null,
             isConfirmButtonEnabled = false,
             isLoading = false,
             textFieldState = TextFieldState.Initial
--- ViewSystemUnidirectionalDataFlow/app/src/main/java/github/io/wottrich/myapplication/udfsample/initial/presentation/viewmodel/InitialViewModel.kt
+++ ViewSystemUnidirectionalDataFlow/app/src/main/java/github/io/wottrich/myapplication/udfsample/initial/presentation/viewmodel/InitialViewModel.kt
@@ -1,54 +1,49 @@
 package github.io.wottrich.myapplication.udfsample.initial.presentation.viewmodel
 
 import androidx.lifecycle.*
-import github.io.wottrich.myapplication.udfsample.initial.domain.ui.InitialUiActions
 import github.io.wottrich.myapplication.udfsample.initial.domain.ui.InitialUiEffects
 import github.io.wottrich.myapplication.udfsample.initial.domain.ui.InitialUiState
 import github.io.wottrich.myapplication.udfsample.initial.domain.usecases.SaveUserUseCase
 import github.io.wottrich.myapplication.livedata.SingleLiveEvent
 import github.io.wottrich.myapplication.textfield.TextFieldState
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.collect
+import github.io.wottrich.myapplication.udfsample.initial.domain.ui.InitialUiStateLiveDataImpl
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class InitialViewModel(
     private val getSaveUserUseCase: SaveUserUseCase
 ) : ViewModel() {
 
-    private val pendingActions = MutableSharedFlow<InitialUiActions>()
-
     private val _uiEffects = SingleLiveEvent<InitialUiEffects>()
     val uiEffects: LiveData<InitialUiEffects> = _uiEffects
 
-    private val _uiState = MutableLiveData<InitialUiState>(InitialUiState.Initial)
-    val uiState: LiveData<InitialUiState> = _uiState
-
-    init {
-        viewModelScope.launch {
-            pendingActions.collect {
-                when (it) {
-                    InitialUiActions.ConfirmName -> saveUser()
-                }
-            }
-        }
-    }
+    private val _uiState = InitialUiStateLiveDataImpl(InitialUiState.Initial)
+    val uiState = _uiState.state
 
     fun onTextChanged(text: String) {
-        _uiState.postValue(uiState.value.getCurrentState(text))
+        changeCurrentState(text)
     }
 
     fun onConfirmButtonClicked() {
-        viewModelScope.launch {
+        saveUser()
+    }
+
+    private fun saveUser() {
+        viewModelScope.launch(Dispatchers.IO) {
             // get current typed text
-            uiState.value?.textFieldState?.text?.let { typedText ->
+            uiState.value.textFieldState.text?.let { typedText ->
                 // validate minimal typed rule
                 if (hasMinimalTypedLength(typedText)) {
                     // if valid post loading
-                    val loadingState = uiState.value?.copy(isLoading = true)
-                    val newState = loadingState.getCurrentState(typedText)
-                    _uiState.postValue(newState)
+                    _uiState.isLoading = true
+                    changeCurrentState(typedText)
                     // and then you will emit action to queue
-                    pendingActions.emit(InitialUiActions.ConfirmName)
+                    uiState.value.textFieldState.text?.let { userName ->
+                        getSaveUserUseCase(userName)
+                        _uiState.isLoading = false
+                        changeCurrentState(typedText)
+                        _uiEffects.postValue(InitialUiEffects.NextScreen)
+                    }
                 } else {
                     // if invalid post a SnackBar with error
                     _uiEffects.postValue(
@@ -62,32 +57,17 @@ class InitialViewModel(
         }
     }
 
-    private fun saveUser() {
-        viewModelScope.launch {
-            val typedText = uiState.value?.textFieldState?.text.orEmpty()
-            uiState.value?.name?.let { userName ->
-                getSaveUserUseCase(userName)
-                val loadingState = uiState.value?.copy(isLoading = false)
-                val newState = loadingState.getCurrentState(typedText)
-                _uiState.postValue(newState)
-                _uiEffects.postValue(InitialUiEffects.NextScreen)
-            }
-        }
-    }
-
-    private fun InitialUiState?.getCurrentState(typedText: String): InitialUiState? {
-        val isLoading = this?.isLoading == true
-        val isConfirmButtonEnabled = typedText.isNotEmpty() && !isLoading
-        // It's important use uiState to not lost other values
-        return this?.copy(
-            name = typedText,
-            isConfirmButtonEnabled = isConfirmButtonEnabled,
+    private fun changeCurrentState(typedText: String) {
+        val isLoading = uiState.value.isLoading
+        val isConfirmButtonEnabledValue = typedText.isNotEmpty() && !isLoading
+        _uiState.apply {
+            isConfirmButtonEnabled = isConfirmButtonEnabledValue
             textFieldState = TextFieldState(
                 text = typedText,
                 errorMessage = getErrorMessageIfNeeded(typedText),
                 isEnabled = !isLoading
             )
-        )
+        }
     }
 
     private fun getErrorMessageIfNeeded(typedText: String): String? {
